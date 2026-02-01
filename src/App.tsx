c7e23ee140dead8e7c318d35999c8f5dae567da0
Vasyl <vasapanov721@gmail.com>
2026-02-01 13:36:59 +0100

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="font-bold">Hello world</div>;
+  return (
+    <div className="font-bold w-full h-screen flex justify-center items-center uppercase text-center p-4 text-7xl text-gray-200">
+      Coming soon
+    </div>
+  );
 }
 
 export default App;
